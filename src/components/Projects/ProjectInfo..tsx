fec91b50022ef1945069c705fbdafb81179b50f7
vibhushakannangara <vibhooshakannangara@gmail.com>
2023-09-01 05:43:45 +0530

    fixed error

--- src/components/Projects/ProjectInfo..tsx
+++ src/components/Projects/ProjectInfo..tsx
@@ -38,7 +38,7 @@ function ProjectInfo() {
   const projectNo = pathnameParts[pathnameParts.length - 1];
 
   useEffect(() => {
-    fetch(`http://52.65.35.114:4002/projectinfo/${projectNo}`)
+    fetch(`http://localhost:4002/projectinfo/${projectNo}`)
       .then((response) => response.json())
       .then((data: ProjectInfoResponse) => {
         setItem(data);
@@ -47,95 +47,9 @@ function ProjectInfo() {
         console.error("Error fetching data:", error);
       });
   }, [projectNo]);
-
+console.log(item)
   return (
-    <div
-      style={{ margin: "40px", width: "90%", left: "2%", position: "relative" }}
-    >
-      {item ? (
-        <div>
-          <MDBCard alignment="center">
-            <MDBCardHeader style={{ fontSize: "30px" }}>
-              <div className="col">
-                <p>{item.projectInfo.altText}</p>
-                <div style={{marginTop:'-20px'}}>
-                  <a
-                  
-                    className="nav-link"
-                    href="https://drive.google.com/file/d/1-q-6zJ1NkXNy6nrrMkMUVNYd_7WF91Wp/view?usp=drive_link"
-                    target="_blank"
-                    rel="noreferrer"
-                  >
-               
-                    <FaGithub></FaGithub>
-                  </a>
-                </div>
-              </div>
-            </MDBCardHeader>
-
-            <MDBCardBody>
-              <MDBRow
-                style={{
-                  display: "flex",
-                  flexDirection: "row",
-                  alignContent: "center",
-                }}
-              >
-                {item.projectInfo.imageUrls.map((item, index) => (
-                  <MDBCol
-                    key={index}
-                    className="mb-4 mb-lg-0"
-                    style={{ margin: "10px" }}
-                  >
-                    <div
-                      className="image-container"
-                      style={{
-                        borderRadius: "10px",
-                        maxHeight: "400px",
-                        width: "auto",
-                        fill: "Background",
-                      }}
-                    >
-                      {" "}
-                      <GlassMagnifier
-                        imageSrc={item}
-                        imageAlt="Example"
-                        largeImageSrc={item} // Optional
-                        magnifierSize={200}
-                        allowOverflow
-                      />
-                    </div>
-                  </MDBCol>
-                ))}
-              </MDBRow>
-            </MDBCardBody>
-          </MDBCard>
-
-          <MDBRow style={{ margin: "10px" }}>
-            <MDBListGroup light  style={{ minWidth: "22rem" }}>
-              {item.projectInfo.text.map((info, index) => (
-               
-                <MDBListGroupItem
-                
-                  key={index} // Use a unique key for each item
-                  className="d-flex justify-content-between align-items-start"
-                >
-                  <div className="ms-2 me-auto">
-                    <div className="fw-bold">
-                    <FaSnowflake style={{color:'green',marginBottom:'3px',marginRight:'5px'}} />
-                      {item.projectInfo.text[index]}
-                    </div>
-                    {info}
-                  </div>
-                </MDBListGroupItem>
-              ))}
-            </MDBListGroup>
-          </MDBRow>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+  <div></div>
   );
 }
 
